f768a35bfd49bb877f78a13d3d7115401e4838ef
사랑이란 멜론 <heebum9588@naver.com>
2024-04-24 23:21:19 +0900

    회원가입 완

--- src/main/kotlin/com/daelim/springtest/main/api/model/dto/userDTO.kt
+++ src/main/kotlin/com/daelim/springtest/main/api/model/dto/userDTO.kt
@@ -1,24 +1,7 @@
 package com.daelim.springtest.main.api.model.dto
 
 data class userData (
-    val userNum: Int = 0,
-    val userName: String = "",
-    val userId: String = "",
-    val userPw: String = "",
-    var login: String ?= "",
-)
-data class addUserData (
-    val userNum: Int = 0,
-    val userName: String = "",
-    val userId: String = "",
-    val userPw: String = "",
-)
-
-data class loginData(
-    val userId: String = "",
-    val userPw: String = "",
-)
-
-data class loginListData(
-    val userValid: String =""
+    val fullName:String = "",
+    val email: String = "",
+    val password: String = ""
 )
