f4850f3befb11fc056d944115d0822babaa088f2
사랑이란 멜론 <heebum9588@naver.com>
2024-04-25 00:38:56 +0900

    lotto add

---
+++ src/main/kotlin/com/daelim/springtest/main/api/model/dto/lottonum.kt
@@ -0,0 +1,9 @@
+package com.daelim.springtest.main.api.model.dto
+
+data class lottonum(
+    var num1:MutableList<Int>,
+    var num2:MutableList<Int>,
+    var num3:MutableList<Int>,
+    var num4:MutableList<Int>,
+    var num5:MutableList<Int>,
+)
--- src/main/kotlin/com/daelim/springtest/main/api/model/dto/userDTO.kt
+++ src/main/kotlin/com/daelim/springtest/main/api/model/dto/userDTO.kt
@@ -1,24 +1,12 @@
 package com.daelim.springtest.main.api.model.dto
 
-data class userData (
-    val userNum: Int = 0,
-    val userName: String = "",
-    val userId: String = "",
-    val userPw: String = "",
-    var login: String ?= "",
-)
-data class addUserData (
-    val userNum: Int = 0,
-    val userName: String = "",
-    val userId: String = "",
-    val userPw: String = "",
-)
 
-data class loginData(
-    val userId: String = "",
-    val userPw: String = "",
+data class userData(
+    val fullName:String = "",
+    val email: String = "",
+    val password: String = ""
 )
-
-data class loginListData(
-    val userValid: String =""
+data class userLoginData (
+    val email: String = "",
+    val password: String = ""
 )
