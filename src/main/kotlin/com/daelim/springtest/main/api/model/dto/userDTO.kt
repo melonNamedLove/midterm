f012caf52eb1f35aa12ce8881932cac327e8d25e
사랑이란 멜론 <heebum9588@naver.com>
2024-04-25 00:54:29 +0900

    1

--- src/main/kotlin/com/daelim/springtest/main/api/model/dto/userDTO.kt
+++ src/main/kotlin/com/daelim/springtest/main/api/model/dto/userDTO.kt
@@ -1,7 +1,12 @@
 package com.daelim.springtest.main.api.model.dto
 
-data class userData (
+
+data class userData(
     val fullName:String = "",
+    val email: String = "",
+    val password: String = ""
+)
+data class userLoginData (
     val email: String = "",
     val password: String = ""
 )
